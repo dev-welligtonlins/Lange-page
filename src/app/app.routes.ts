9b93834b79ab9cce8e7c85f2064e055f2ef9f455
dev-welligtonlins <welligtonlinsemp@gmail.com>
2025-07-30 10:27:57 -0300

    #1 subindo projeto

---
+++ src/app/app.routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from '@angular/router';
+import { HomePage } from './pages/home-page/home-page';
+
+export const routes: Routes = [
+    {path: '', loadComponent: () => import('./pages/home-page/home-page').then(m => m.HomePage)}
+];
